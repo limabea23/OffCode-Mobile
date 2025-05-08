ebdab9aaee0fef08b46087d4b320fe829d12cb5c
Beatriz L <158210831+limabea23@users.noreply.github.com>
2025-05-08 16:07:37 -0300

    feat: loading.js component

---
+++ components/Loading.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, Image, View } from 'react-native';
+
+export default function Loading() {
+    return (
+    <View style={styles.container}>
+        <Image style={styles.image} source={require('./assets/public/on-off.gif')} />
+        <Text style={styles.text}>Carregando...</Text>
+        <StatusBar style="auto" />
+    </View>
+);
+}
+
+const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+},
+    image: {
+    borderRadius: 50,
+    borderColor: '#fff',
+    width: 250,
+    height: 250,
+},
+    text: {
+    color: '#fff',
+    fontSize: 25,
+}
+});
