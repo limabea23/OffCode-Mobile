13f70b8844efd53aa5d3597c9811b6ff18723f82
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-05-13 16:17:54 -0300

    feat: add vertical line

--- components/Conexoes.js
+++ components/Conexoes.js
@@ -6,8 +6,10 @@ export default function Conexoes({seguindo, seguidores, postagens}) {
         <View style={styles.card}>
             <Text style={styles.p}>SEGUINDO</Text>
             <Text style={styles.seguindo}>{seguindo}</Text>
+            <View style={styles.linhaVertical} />
             <Text style={styles.p}>SEGUIDORES</Text>
             <Text style={styles.seguidores}>{seguidores}</Text>
+            <View style={styles.linhaVertical} />
             <Text style={styles.p}>POSTAGENS</Text>
             <Text style={styles.postagens}>{postagens}</Text>
         </View>
@@ -21,9 +23,22 @@ const styles = StyleSheet.create ({
     borderRadius: 10,
     padding: 15,
     maxWidth: 300,
-    minHeight: 150,
+    height: 90,
+    display: 'flex',
+    flexDirection: 'row',
+    marginLeft: 25,
+    marginTop: 15,
     },
     p: {
         color: '#fff',
+        fontWeight: 'bold',
+        margin: 5,
+    },
+    linhaVertical: {
+        width: 1, 
+        height: 50,
+        backgroundColor: '#fff', 
+        alignSelf: 'center', 
+        marginTop: 5, 
     },
 })
