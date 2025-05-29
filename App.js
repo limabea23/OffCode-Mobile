72feb9ccd6411363d0164edcf122f71843bb96fd
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2025-05-29 14:24:16 -0300

    feat: Update page Noticias.js

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import Post from './pages/Post';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Post />
+      <Noticias />
       <StatusBar style="light" />
     </View>
   );
